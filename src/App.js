e00fea6376441cb9c0590ec68ddf8aec88df30fe
German <fuchsoria@protonmail.ch>
2022-02-23 23:50:39 +0100

    Add music metadata test case

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback } from "react";
+import * as mm from "music-metadata-browser";
+import { useDropzone } from "react-dropzone";
 
-function App() {
+function MyDropzone() {
+  const onDrop = useCallback((acceptedFiles) => {
+    const file = acceptedFiles[0];
+
+    const metadata = mm.parseReadableStream(file.stream());
+
+    metadata.then((result) => {
+      console.log(result);
+    });
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <p>Drop the files here ...</p>
+      ) : (
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      )}
     </div>
   );
 }
 
-export default App;
+export default function App() {
+  return <MyDropzone />;
+}
